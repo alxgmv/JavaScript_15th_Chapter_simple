cc55ab7bff2b8bc7d09f721dbca638bbd1d9c810
alxgmv <alxgmv@gmail.com>
2020-05-03 14:40:24 +0200

    2nd

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 class Comment extends React.Component {
   constructor() {
     super();
-    this.state = {
-      options: [
+    this.state =
+      {
+        fields : [
         { author: 'moderator' },
         { text: 'test message' },
-      ]
-    };
-    {
-      date: new Date()
-    };
+      ],
+      newCommentText:''
+    }
+  }
 
+  addCommentText(value) {
+    const fields = this.state.fields;
+    fields.push({
+      author: this.state.author,
+      text: this.state.newCommentText
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>
+        <div>
           {
-            this.state.options.map(options => {
-              return(
-                <div>
-                  {options.author}
-                  {options.text}
-                </div>
+            this.state.fields.map(field =>{
+              return (
+                <h1>
+                  {field.author}
+                  {field.text}
+                </h1>
               )
             })
           }
-          </h1>
-          <h2>
-            It is {this.state.date.toLocaleTimeString()}.
-          </h2>
+        </div>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Comment text"
+            value={this.state.newCommentText}
+            onChange={ev => {
+              this.setState({ newCommentText :ev.target.value});
+            }}
+            onKeyDown={ev => {
+              if (ev.keyCode ===13) {
+                this.addCommentText();
+              }
+            }}
+
+          />
+        </div>
       </div>
     );
   }
@@ -39,3 +59,7 @@ ReactDOM.render(
   <Comment />,
   document.getElementById('app')
 );
+// <h2>
+//      {this.state.date.toLocaleTimeString()}
+// </h2>
+// {  date: new Date()},
